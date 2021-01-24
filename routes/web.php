1efb686975eb199493181d497d191fa13bc15794
mina <mn.memar@gmail.com>
2021-01-24 22:15:32 +0330

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home','HomeController@sayHi');
-Route::resource('/example','ExampleController');
+Route::resource('/example','ExampleController')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
